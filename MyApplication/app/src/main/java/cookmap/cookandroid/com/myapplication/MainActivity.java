ac9ef7674c2d078bee9e0aac44ebedcb34fe78fc
mycsk123 <mycsk123@gmail.com>
2017-08-30 18:00:31 +0900

    20170830 2st

---
+++ MyApplication/app/src/main/java/cookmap/cookandroid/com/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package cookmap.cookandroid.com.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions().position(new LatLng(35.134004, 129.103702)).title("부경대"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.134004, 129.103702), 15));
+    }
+}
